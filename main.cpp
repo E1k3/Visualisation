873b7fee4a316cef8b5d571c33ed213b258415a6
Eike Gebauer <e_geba02@wwu.de>
2017-05-04 14:57:23 +0200

    Undid OpenGl context change to widen support.

--- main.cpp
+++ main.cpp
@@ -60,8 +60,8 @@ int main(int argc, char *argv[])
 		throw std::runtime_error("GLFW init failed");
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window = glfwCreateWindow(1000, 1000, "Test", NULL, NULL);
