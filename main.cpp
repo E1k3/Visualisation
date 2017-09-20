f7de36dd7e116174f9411c78c6aa6c50f87557a6
Eike Gebauer <e_geba02@wwu.de>
2017-09-20 02:31:16 +0200

    Commented-out the testing lines in main.

--- main.cpp
+++ main.cpp
@@ -11,20 +11,20 @@ using namespace vis;
 
 int main(int /*argc*/, char */*argv*/[])
 {
-	auto ifs = std::ifstream{"/home/eike/Documents/Uni/Bachelor/python/data/sample.dat"};
-	auto sample = std::vector<float>{};
-	auto line = std::string{};
-	while(std::getline(ifs, line))
-	{
-		sample.push_back(std::stof(line));
-		std::cout << sample.back() << '\n';
-	}
-	std::cout << '\n';
-	std::sort(sample.begin(), sample.end());
-	auto gmms = math_util::fit_gmm(sample, 4);
-	for(const auto& gmm : gmms)
-		std::cout << gmm._mean << ' ' << std::sqrt(gmm._variance) << ' ' << gmm._weight << '\n';
-	return 0;
+//	auto ifs = std::ifstream{"/home/eike/Documents/Uni/Bachelor/python/data/sample.dat"};
+//	auto sample = std::vector<float>{};
+//	auto line = std::string{};
+//	while(std::getline(ifs, line))
+//	{
+//		sample.push_back(std::stof(line));
+//		std::cout << sample.back() << '\n';
+//	}
+//	std::cout << '\n';
+//	std::sort(sample.begin(), sample.end());
+//	auto gmms = math_util::fit_gmm(sample, 4);
+//	for(const auto& gmm : gmms)
+//		std::cout << gmm._mean << ' ' << std::sqrt(gmm._variance) << ' ' << gmm._weight << '\n';
+//	return 0;
 
 	try
 	{
